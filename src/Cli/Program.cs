1f1b446f4ac69f4f01743d78f8133b63774d443e
Sofiia Rakovych <-email---github@example.com>
2026-09-05 18:04:04 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices; 
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування"); 
+Console.WriteLine("Студент: Ракович Софія, група ФЕІ-32"); 
+Console.WriteLine(new string('-', 52)); 
+Console.WriteLine($"ОС (OSDescription)   : {RuntimeInformation.OSDescription}"); 
+Console.WriteLine($"ОС (Environment)     : {Environment.OSVersion}"); 
+Console.WriteLine($"Архітектура процесу  : {RuntimeInformation.ProcessArchitecture}"); 
+Console.WriteLine($"Версія .NET (CLR)    : {Environment.Version}"); 
+Console.WriteLine($"Runtime              : {RuntimeInformation.FrameworkDescription}"); 
+Console.WriteLine($"Каталог застосунку   : {AppContext.BaseDirectory}"); 
+Console.WriteLine($"Поточний каталог     : {Environment.CurrentDirectory}"); 
+Console.WriteLine(new string('-', 52)); 
+Console.WriteLine("Предметна область: Склад (товари, партії, залишки, переміщення)");
