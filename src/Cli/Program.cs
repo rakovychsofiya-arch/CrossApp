b1fdd0dd12af62f814359ba63e6dde9444cfec43
Sofiia Rakovych <-email---github@example.com>
2026-09-14 18:25:30 +0300

    Format JSON output for better readability

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,14 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.InteropServices; 
-Console.WriteLine("CrossApp – практикум з крос-платформного програмування"); 
-Console.WriteLine("Студент: Ракович Софія, група ФЕІ-32"); 
-Console.WriteLine(new string('-', 52)); 
-Console.WriteLine($"ОС (OSDescription)   : {RuntimeInformation.OSDescription}"); 
-Console.WriteLine($"ОС (Environment)     : {Environment.OSVersion}"); 
-Console.WriteLine($"Архітектура процесу  : {RuntimeInformation.ProcessArchitecture}"); 
-Console.WriteLine($"Версія .NET (CLR)    : {Environment.Version}"); 
-Console.WriteLine($"Runtime              : {RuntimeInformation.FrameworkDescription}"); 
-Console.WriteLine($"Каталог застосунку   : {AppContext.BaseDirectory}"); 
-Console.WriteLine($"Поточний каталог     : {Environment.CurrentDirectory}"); 
-Console.WriteLine(new string('-', 52)); 
-Console.WriteLine("Предметна область: Склад (товари, партії, залишки, переміщення)");
+
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+var envData = new
+{
+    OSDescription = RuntimeInformation.OSDescription,
+    EnvironmentOS = Environment.OSVersion.ToString(),
+    ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
+    DotNetVersion = Environment.Version.ToString(),
+    FrameworkDescription = RuntimeInformation.FrameworkDescription,
+    BaseDirectory = AppContext.BaseDirectory,
+    CurrentDirectory = Environment.CurrentDirectory
+};
+
+if (args.Contains("--json"))
+{   
+     var options = new JsonSerializerOptions 
+    { 
+        WriteIndented = true 
+    };
+    string json = JsonSerializer.Serialize(envData,options);
+    Console.WriteLine(json);
+}
+else
+{
+    Console.WriteLine("================================================================================");
+    Console.WriteLine($"| {"Параметр",-25} | {"Значення",-48} |");
+    Console.WriteLine("================================================================================");
+    Console.WriteLine($"| {"ОС (OSDescription)",-25} | {envData.OSDescription,-48} |");
+    Console.WriteLine($"| {"ОС (Environment)",-25} | {envData.EnvironmentOS,-48} |");
+    Console.WriteLine($"| {"Архітектура процесора",-25} | {envData.ProcessArchitecture,-48} |");
+    Console.WriteLine($"| {"Версія .NET (CLR)",-25} | {envData.DotNetVersion,-48} |");
+    Console.WriteLine($"| {"Runtime",-25} | {envData.FrameworkDescription,-48} |");
+    Console.WriteLine($"| {"Каталог застосунку",-25} | {envData.BaseDirectory,-48} |");
+    Console.WriteLine($"| {"Поточний каталог",-25} | {envData.CurrentDirectory,-48} |");
+    Console.WriteLine("================================================================================");
+}
